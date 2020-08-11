2f9bf50efb664a49db25216f02b38eaf000ebd91
Thomas Mendez <thomasmendez01@gmail.com>
2020-08-11 15:50:23 -0500

    Update navbar.component.spec.ts
    Added test case

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavbarComponent } from './navbar.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -26,4 +27,10 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain nav html tag', () => {
+    let de = fixture.debugElement;
+    expect(de.query(By.css('nav'))).toBeTruthy();
+  });
+
 });
